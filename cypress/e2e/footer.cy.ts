05f881d84b3d101706bbec22bd33ef363a60062e
gracedward <edward.chung.designer@gmail.com>
2022-12-01 00:23:55 +0000

    added a footer to each page with version, links and logo

---
+++ cypress/e2e/footer.cy.ts
@@ -0,0 +1,46 @@
+describe("Sidebar Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/dashboard");
+  });
+
+  context("desktop resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1025, 900);
+    });
+
+    it("footer links are working", () => {
+      // check that each link leads to the correct page
+      cy.get("footer").contains("Docs").should("have.attr", "href", "/docs");
+
+      cy.get("footer").contains("API").should("have.attr", "href", "/api");
+
+      cy.get("footer").contains("Help").should("have.attr", "href", "/help");
+
+      cy.get("footer")
+        .contains("Community")
+        .should("have.attr", "href", "/community");
+    });
+
+    it("has the correct version number", () => {
+      cy.get("footer").contains(
+        `Version: ${Cypress.env("NEXT_PUBLIC_APP_VERSION")}`
+      );
+    });
+
+    it("has the logo as background", () => {
+      cy.get("footer span")
+        .last()
+        .invoke("css", "background-image")
+        .should("contain", "/icons/logo-small.svg");
+    });
+  });
+
+  context("mobile resolution", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-8");
+    });
+    it("version should have an order property", () => {
+      cy.get("footer").contains("Version:").should("have.css", "order", "1");
+    });
+  });
+});
