f9e9b985d4baca9511df6306adbaf89ecc05a998
gracedward <edward.chung.designer@gmail.com>
2022-12-07 21:53:43 +0000

    feat: Landing Page header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { Routes } from "@config/routes";
+import { color, textFont, space } from "@styles/theme";
+
+import Link from "next/link";
 
 const Header = styled.header`
   width: 100%;
@@ -28,13 +31,49 @@ const ContactButton = styled.button`
   }
 `;
 
+const Navigation = styled.nav`
+  ${textFont("md", "medium")}
+
+  & a {
+    color: ${color("gray", 500)};
+    padding: 28px 16px;
+    text-decoration: none;
+  }
+  & a:hover {
+    color: ${color("primary", 700)};
+  }
+`;
+
+const OpenDashboardLink = styled.a`
+  ${textFont("md", "medium")}
+  background: ${color("primary", 600)};
+  padding: 10px 18px;
+  border-radius: 8px;
+  border: 1px solid ${color("primary", 600)};
+  color: #fff;
+  text-decoration: none;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  flex-shrink: 0;
+  :hover {
+    background-color: ${color("primary", 700)};
+  }
+`;
+
 const IssuesPage = () => {
   return (
     <div>
       <Header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <Navigation>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/documentation">Documentation</Link>
+          <Link href="/pricing">Pricing</Link>
+        </Navigation>
+        <OpenDashboardLink href={Routes.projects}>
+          Open Dashboard
+        </OpenDashboardLink>
       </Header>
       <ContactButton
         onClick={() =>
